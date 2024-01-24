a129ab84a7cf3be4e4f660ec4b37408b51605fbc
Noizzzz-Alex <Noizzzz@yandex.ru>
2024-01-24 18:01:30 +0700

    Initial Commit

---
+++ FinalWorkOBJ_C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FinalWorkOBJ_C
+//
+//  Created by Александр Харлампов on 24.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
